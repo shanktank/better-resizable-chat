7c9fdd60bde8ab1c9da98c3a71e47086442a726b
Shanktank <5628208+shanktank@users.noreply.github.com>
2026-06-17 08:25:11 -0500

    update description and tags

--- src/main/java/brc/BetterResizableChatPlugin.java
+++ src/main/java/brc/BetterResizableChatPlugin.java
@@ -35,7 +35,7 @@ import javax.swing.SwingUtilities;
 @PluginDescriptor(
     name = "Better Chat Resizer",
     description = "Resize chat (requires resizable layout)",
-    tags = {"chat", "chatbox", "extend", "resize", "scale", "stretch", "width", "height", "ui"}
+    tags = {"chat", "chatbox", "private", "message", "extend", "resize", "scale", "stretch", "width", "height", "ui"}
 )
 public class BetterResizableChatPlugin extends Plugin {
     private static final int TOPLEVEL_RELAYOUT_SCRIPT = 1972;
